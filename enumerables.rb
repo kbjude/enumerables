0b1699a55550d1be707640d824915ee0ca8f9545
kbjude <kbjude@gmail.com>
2020-03-30 12:44:27 +0300

    my_select WIP

--- enumerables.rb
+++ enumerables.rb
@@ -16,5 +16,13 @@ module Enumerable
   end
 [1,2,3,4,5].my_each_with_index {| v, i | puts "#{i} => #{v}"}
 
-
+def my_select
+  arr = self
+  for i in arr
+      if i > 2
+      yield i
+      end
+  end
+end
+[3,4,1,8,4,2,5].my_select  { |i| puts i}
 end
