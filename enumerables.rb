745030a9ea4fd9e17108154d0fca089348e77c57
kbjude <kbjude@gmail.com>
2020-03-31 14:19:05 +0300

    my_any

--- enumerables.rb
+++ enumerables.rb
@@ -50,4 +50,16 @@ def my_any(argument = nil)
     true
     end
   end
+  def my_any(argument = nil)
+    arr = self
+    if block_given?
+      arr.my_each do |i|
+      return false
+      end
+    else
+      my_each { |i| yield i if i.nil? || i == false }
+    end
+    true
+    end
+  end
 end
