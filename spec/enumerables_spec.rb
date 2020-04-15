c4fd813dd85d98f986d65d61d7e751cd978f100b
kbjude <kbjude@gmail.com>
2020-04-16 02:34:13 +0300

    fixing with_index bugs

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -20,17 +20,15 @@ RSpec.describe Enumerable do
 
     describe '#.my_each_with_index' do
 
-    context 'When array and block are given'do
-        it 'returns the items in the array' do
-            expect { arr.my_each { |i, v| print i, v }}.to output('12343').to_stdout
+        context 'When array and block are given'do
+            it 'returns the items in the array' do
+                expect { arr.my_each { |i, v| print i, v }}.to output('12343').to_stdout
+            end
         end
-    end
-    context 'When no block is given' do
-        it 'returns an enumerator' do
-            expect(arr.my_each).to be_instance_of(Enumerator)
+        context 'When no block is given' do
+            it 'returns an enumerator' do
+                expect(arr.my_each).to be_instance_of(Enumerator)
+            end
         end
-    end
-end
-
     end
 end
