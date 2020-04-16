14264cd05d07f139a59bb63cbedcb1b30608f71e
kbjude <kbjude@gmail.com>
2020-04-16 03:18:25 +0300

    myselrct all set

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,6 +2,7 @@ require './lib/enumerables'
 
 RSpec.describe Enumerable do
     let(:arr) { [1, 2, 3, 4, 3] }
+    let(:new_arr) { [ ] }
 
 
     describe '#.my_each' do
@@ -21,14 +22,27 @@ RSpec.describe Enumerable do
     describe '#.my_each_with_index' do
 
         context 'When array and block are given'do
-            it 'returns the items in the array' do
-                expect { arr.my_each { |i, v| print i, v }}.to output('12343').to_stdout
+            it 'returns each item in the array with its index' do
+                expect { arr.my_each_with_index { |i, v| print i, v }}.to output('1021324332').to_stdout
             end
         end
         context 'When no block is given' do
             it 'returns an enumerator' do
-                expect(arr.my_each).to be_instance_of(Enumerator)
+                expect(arr.my_each_with_index).to be_instance_of(Enumerator)
             end
         end
     end
+    describe '#.my_select' do
+
+        context 'When array and block are given'do
+            it 'returns the selected items in the array' do
+                 expect( arr.my_select { |i| i > 3 }).to eql([4])
+            end
+        end
+        context 'When no block is given' do
+            it 'returns an enumerator' do
+                 expect( arr.my_select {|i| i == 3 }).to eql( [ 3, 3 ] )
+            end
+        end
+     end
 end
